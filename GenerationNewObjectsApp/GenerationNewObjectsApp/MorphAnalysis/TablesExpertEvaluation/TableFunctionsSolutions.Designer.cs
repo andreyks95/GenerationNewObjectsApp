c69420112ebecd5ba3c4bfc665755f0e0e58c4de
Andrey <andreyks95@gmail.com>
2018-09-20 23:19:02 +0300

    new form working with two dgv

---
+++ GenerationNewObjectsApp/GenerationNewObjectsApp/MorphAnalysis/TablesExpertEvaluation/TableFunctionsSolutions.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace MorphAnalysis.TablesExpertEvaluation
+{
+    partial class TableFunctionsSolutions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(277, 188);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(283, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(505, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.RowStateChanged += new System.Windows.Forms.DataGridViewRowStateChangedEventHandler(this.dataGridView1_RowStateChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(0, 243);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(277, 185);
+            this.panel2.TabIndex = 2;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(295, 243);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(493, 185);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // TableFunctionsSolutions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "TableFunctionsSolutions";
+            this.Text = "TableFunctionsSolutions";
+            this.Load += new System.EventHandler(this.TableFunctionsSolutions_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
---
+++ GenerationNewObjectsApp/GenerationNewObjectsApp/MorphAnalysis/TablesExpertEvaluation/TableFunctionsSolutions.cs
@@ -0,0 +1,101 @@
+﻿using MorphAnalysis.HelperClasses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MorphAnalysis.TablesExpertEvaluation
+{
+    public partial class TableFunctionsSolutions : Form
+    {
+        private MorphModel db;
+
+        private CacheData cacheData = CacheData.GetInstance();
+
+        //Отримаємо локальний список функцій та їх рішень
+        private List<SolutionsOfFunction> solOfFuncList;
+
+        //Отримаємо локальний список функцій
+        private List<Function> funcList;
+
+        //Отримаємо кількість експертів
+        private int countOfExpert = 1;
+
+        public TableFunctionsSolutions()
+        {
+            InitializeComponent();
+
+            db = new MorphModel();
+
+            solOfFuncList = cacheData.getListSolutionOfFunction;
+
+            funcList = cacheData.getListFunction;
+
+        }
+
+        public TableFunctionsSolutions(int countOfExpert) : this()
+        {
+            this.countOfExpert = countOfExpert;
+
+            //dataGridView1.Columns.Add("Functions", "Функції");
+            //for (int i = 0; i < countOfExpert; i++)
+            //    dataGridView1.Columns.Add("Expert" + i, "Експ. № " + (i + 1));
+        }
+
+        private void FillDgvExperts(DataGridView dgv, string nameFirstColumn)
+        {
+            dgv.Columns.Add("FirstCol", nameFirstColumn);
+            for (int i = 0; i < countOfExpert; i++)
+                dgv.Columns.Add("Expert" + i, "Експ. № " + (i + 1));
+        }
+
+        private void TableFunctionsSolutions_Load(object sender, EventArgs e)
+        {
+            FillDgvExperts(dataGridView1, "Функції");
+            FillDgvExperts(dataGridView2, "Рішення функції");
+
+            foreach (var list in funcList)
+                dataGridView1.Rows.Add(list.name);
+
+            //відображаємо в dgv функції: ім'я, характеристика, вага
+            //dataGridView1.DataSource = funcList.Select(f => new { f.name, f.characteristics, f.weight } ).ToList();
+        }
+
+        private void dataGridView1_RowStateChanged(object sender, DataGridViewRowStateChangedEventArgs e)
+        {
+            if (e.Row.Index >= 0)
+            {
+                //Исключение здесь возникло
+                string firstCellValue = e.Row.Cells[0].Value ?. ToString(); ;
+
+                if (firstCellValue is null) return;
+
+                //string firstCellValue = e.Row.Cells[0].Value.ToString();
+
+                //отримаємо функцію для запиту
+                Function selectedFunc = funcList.FirstOrDefault(f => f.name == firstCellValue);
+
+                //запит де зіставляємо рішення які зберігаються в БД
+                // з рішеннями і функціями в ОЗУ. Дістаємо ім'я рішення та іншу інформацію
+                var getPartQuerySolutionsWithFunction = from sol in db.Solutions.ToList()
+                                                        join sf in solOfFuncList on sol.id_solution equals sf.Solution.id_solution
+                                                        select new { sol.id_solution, sol.name, sol.weight, id_func = sf.Function.id_function };
+
+                var getResult = from sol in getPartQuerySolutionsWithFunction
+                                where selectedFunc.id_function == sol.id_func
+                                select new { sol.name, sol.weight };
+
+
+                //TODO: Подумать как правильно отобразить данные здесь
+                //TODO: сделать проверку ячеек на ввод только чисел (можно и дабл можно и инт)
+                //Как потом вытащить оценки? Как их нормировать? Как сново отобразить новый столбец с нормированными усредёнными оценками?
+               // this.dataGridView2.DataSource = getResult.ToList();
+            }
+        }
+    }
+}
