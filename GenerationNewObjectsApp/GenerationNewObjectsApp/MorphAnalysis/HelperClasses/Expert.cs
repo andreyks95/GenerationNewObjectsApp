3f487abc4c13463d7e6f083d55eb642146be7bca
Andrey <andreyks95@gmail.com>
2018-09-21 17:42:15 +0300

    create Expert class which storage owners estimates from column dgv

---
+++ GenerationNewObjectsApp/GenerationNewObjectsApp/MorphAnalysis/HelperClasses/Expert.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorphAnalysis.HelperClasses
+{
+    public class Expert
+    {
+
+        private double[] values = new double[0];
+
+        private void setEstimates()
+        {
+
+        }
+
+        public void AddValue(double value)
+        {
+            double[] newList = new double[values.Length + 1];
+            for (int i = 0; i < values.Length; i++)
+            {
+                newList[i] = values[i];
+            }
+            newList[newList.Length - 1] = value;
+            values = newList;
+        }
+
+        public double[] getEstimates => values;
+    }
+}
