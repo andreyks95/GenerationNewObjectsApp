e5b199476a527b5b9e12fb584755a333c79b8c3c
Andrey <andreyks95@gmail.com>
2018-09-21 17:42:43 +0300

    created class for calc and normalizer estimates which have Experts

---
+++ GenerationNewObjectsApp/GenerationNewObjectsApp/MorphAnalysis/HelperClasses/Normalizer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorphAnalysis.HelperClasses
+{
+
+    class Normalizer
+    {
+        //2-й метод
+        //TODO: принять експерта или список експертов
+        //Пробежаться по каждому експерту который будет содержать список оценко по столбцам
+        //Нормализировать оценки (сумма на елемент)
+        //Присвоить новые оценки експерту 
+
+        //Просто нормализовать оценки передав ему массив 
+
+        //Вычислить среднее для строк (элементов)
+    }
+}
