ba59b5f6a8964937d71ed8c9f18744a33c94629b
Andrey <andreyks95@gmail.com>
2018-09-24 09:57:34 +0300

    Add method CalcAvg, CalcNormalize

--- GenerationNewObjectsApp/GenerationNewObjectsApp/MorphAnalysis/HelperClasses/Normalizer.cs
+++ GenerationNewObjectsApp/GenerationNewObjectsApp/MorphAnalysis/HelperClasses/Normalizer.cs
@@ -9,14 +9,51 @@ namespace MorphAnalysis.HelperClasses
 
     class Normalizer
     {
-        //2-й метод
-        //TODO: принять експерта или список експертов
-        //Пробежаться по каждому експерту который будет содержать список оценко по столбцам
-        //Нормализировать оценки (сумма на елемент)
-        //Присвоить новые оценки експерту 
 
-        //Просто нормализовать оценки передав ему массив 
+        //Нормалізація оцінок по стовбцям для списка експертів
+        public List<Expert> CalcNormalizeExpertsEstimates(List<Expert> listExpets)
+        {
+            List<Expert> newValues = new List<Expert>();
+            foreach (var item in listExpets)
+            {
+                double[] value = CalcNormalizeExpertEstimates(item);
+                Expert exp = new Expert();
+                exp.AddValueRange(value);
+                newValues.Add(exp);
+            }
+            return newValues;
+
+        }
+
+        //Нормалізація оцінок по стовбцю для експерта
+        public double[] CalcNormalizeExpertEstimates(Expert expert) => CalcNormalizeEstimates(expert.getEstimates);
+        
+
+
+        //Нормалізація оцінок по масиву
+        public double[] CalcNormalizeEstimates(double[] values)
+        {
+            double[] buf = values;
+            double[] newValues = new double[buf.Length];
+            double sum = 0;
+
+            for (int i = 0; i < buf.Length; i++)
+            {
+                sum += buf[i];
+            }
+
+            for (int i = 0; i < buf.Length; i++)
+            {
+                newValues[i] = buf[i] / sum;
+            }
+
+            return newValues;
+        }
 
         //Вычислить среднее для строк (элементов)
+
+        public double CalcAvg(double[] values) => values.Average();
+        
+
     }
 }
