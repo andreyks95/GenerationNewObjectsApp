ae362b152b0d52505d282dcaa8e0986d0fcfb1db
Andrey <andreyks95@gmail.com>
2018-09-28 09:28:12 +0300

    Add new Table for Parameters Goals Solution

---
+++ GenerationNewObjectsApp/GenerationNewObjectsApp/MorphAnalysis/TablesExpertEvaluation/TableParametersGoalsSolutions.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace MorphAnalysis.TablesExpertEvaluation
+{
+    partial class TableParametersGoalsSolutions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSaveRating = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.Desktop;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.Lime;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.Desktop;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(784, 511);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.dataGridView1_CellValidating);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.buttonSaveRating);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.MinimumSize = new System.Drawing.Size(0, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 50);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Параметрична формула розрахунку виконання\r\nтехнічними рішеннями згідно параметрам" +
+    " цілей";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonSaveRating
+            // 
+            this.buttonSaveRating.Location = new System.Drawing.Point(257, 12);
+            this.buttonSaveRating.Name = "buttonSaveRating";
+            this.buttonSaveRating.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveRating.TabIndex = 0;
+            this.buttonSaveRating.Text = "Розрахунок";
+            this.buttonSaveRating.UseVisualStyleBackColor = true;
+            this.buttonSaveRating.Click += new System.EventHandler(this.buttonSaveRating_Click);
+            // 
+            // TableParametersGoalsSolutions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "TableParametersGoalsSolutions";
+            this.Text = "TableParametersGoalsSolutions";
+            this.Load += new System.EventHandler(this.TableParametersGoalsSolutions_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSaveRating;
+    }
+}
---
+++ GenerationNewObjectsApp/GenerationNewObjectsApp/MorphAnalysis/TablesExpertEvaluation/TableParametersGoalsSolutions.cs
@@ -0,0 +1,151 @@
+﻿using MorphAnalysis.HelperClasses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MorphAnalysis.TablesExpertEvaluation
+{
+    public partial class TableParametersGoalsSolutions : Form
+    {
+        private CacheData cacheData = CacheData.GetInstance();
+
+        private ConfigDGV configDGV;
+
+        //Параметри цілей
+        List<ParametersGoal> parametersGoalsList;
+
+        //Рішення з морф. таблиці
+        private List<SolutionsOfFunction> solOfFuncList;
+
+        //Для тимчасового зберігання назв рішень
+        private string solutionNameInTable;
+
+        //Для тимчасового зберігання назв функції
+        private string functionNameInTable;
+
+        List<decimal> ratings = new List<decimal>();
+
+
+        public TableParametersGoalsSolutions()
+        {
+            InitializeComponent();
+
+            solOfFuncList = cacheData.getListSolutionOfFunctionMorphTable;
+
+            parametersGoalsList = cacheData.getListParameterGoalForTables;
+
+            configDGV = new ConfigDGV();
+
+        }
+
+        private void TableParametersGoalsSolutions_Load(object sender, EventArgs e)
+        {
+
+
+            //Наповнюємо стовбцями таблицю
+            dataGridView1.Columns.Add("FirstCol", "Тех. рішення / Параметри цілей ");
+
+            int i = 0;
+            foreach (ParametersGoal param in parametersGoalsList)
+            {
+                dataGridView1.Columns.Add("P" + ++i, "Параметр: " + param.name + " : " + " Ціль: "+ param.Goal.name + " " 
+                    + "ср. знач. " + param.avg + " " + param.unit);
+            }
+            dataGridView1.Columns.Add("Sum", "Оцінка");
+            dataGridView1.Columns[dataGridView1.Columns.Count - 1].Visible = false;
+
+            //Стовбці тільки для читання
+            configDGV.SetColumnsDgvOnlyRead(dataGridView1, new[] { 0, dataGridView1.Columns.Count - 1 });
+            
+            
+            //Наповнюємо рядками таблицю
+            foreach (SolutionsOfFunction solOfFunc in solOfFuncList)
+            {
+                dataGridView1.Rows.Add(solOfFunc.Solution.name + " & " + solOfFunc.Function.name);
+            }
+        }
+
+        private void dataGridView1_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            int value;
+            configDGV.CellValidating(dataGridView1, sender, e);
+            if (int.TryParse(Convert.ToString(e.FormattedValue), out value))
+                if (!(value >= 0 && value <= 300))
+                {
+                    MessageBox.Show("Значення комірки повинно бути: [0;100]!", "Помилка вводу");
+                    dataGridView1.CurrentCell.Value = 0;
+                }
+
+        }
+
+
+        //Збереження оцінок з DGV для кожного рішення параметру цілі
+        //Параметрична формула розрахунку виконання
+        //технічними рішеннями згідно параметрам цілей
+        private void buttonSaveRating_Click(object sender, EventArgs e)
+        {
+            //Переделать ЗДЕСЬ!!!
+
+            //int firstIndex = dataGridView1.Columns.GetFirstColumn(DataGridViewElementStates.Visible, DataGridViewElementStates.None).Index;
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                GetFirstColumnNameSolutionFunction(i, ref solutionNameInTable, ref functionNameInTable);
+
+                //Знайдемо рішення і функцію, яке зараз оцінюється відносно функцій
+                SolutionsOfFunction selectedSolOfFunc = solOfFuncList.FirstOrDefault(s => (s.Solution.name == solutionNameInTable) && (s.Function.name == functionNameInTable));
+                if (selectedSolOfFunc is null) return;
+
+                decimal estimateSol = selectedSolOfFunc.Solution.weight ?? 0;
+
+                int index = 0;
+                decimal sum = 0;
+
+                //Отримаємо оцінки по стовбцям 
+                foreach (Function func in funcList)
+                {
+                    decimal estimateFunc = func.weight ?? 0;
+                    decimal estimateSolOnFunc = 0;
+                    if (!(decimal.TryParse(dataGridView1[++index, i].Value.ToString(), out estimateSolOnFunc)))
+                    {
+                        MessageBox.Show("Неможливо конвертувати значення комірки в тип decimal", "Помилка");
+                        return;
+                    }
+                    sum += (estimateFunc * (estimateSolOnFunc / 100.0m));
+                }
+
+                decimal finalEstimate = estimateSol * sum;
+
+                selectedSolOfFunc.rating = finalEstimate;
+                dataGridView1[dataGridView1.Columns.Count - 1, i].Value = finalEstimate;
+                //ratings.Add(finalEstimate);
+            }
+            dataGridView1.Columns[dataGridView1.Columns.Count - 1].Visible = true;
+
+        }
+
+        private void GetFirstColumnNameSolutionFunction(int indexRow, ref string sol, ref string func)
+        {
+            int firstIndex = dataGridView1.Columns.GetFirstColumn(DataGridViewElementStates.Visible, DataGridViewElementStates.None).Index;
+
+            string valueFirstColumn = Convert.ToString(dataGridView1[firstIndex, indexRow].Value);
+            valueFirstColumn = valueFirstColumn.Trim();
+            if (valueFirstColumn == null || valueFirstColumn == "") return;
+            string[] solFuncFirstColumn = valueFirstColumn.Split(new char[] { '&' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int indexString = 0; indexString < solFuncFirstColumn.Length; indexString++)
+                solFuncFirstColumn[indexString] = solFuncFirstColumn[indexString].Trim();
+
+            if (solFuncFirstColumn[0] == null || solFuncFirstColumn[0] == "") return;
+
+            sol = solFuncFirstColumn[0];
+            func = solFuncFirstColumn[1];
+
+        }
+
+    }
+}
