2cca818e0bdae0c0f59d734d7b78d98f7321dfea
Andrey <andreyks95@gmail.com>
2018-09-28 16:36:25 +0300

    using GetList<>()

--- GenerationNewObjectsApp/GenerationNewObjectsApp/MorphAnalysis/Form1.cs
+++ GenerationNewObjectsApp/GenerationNewObjectsApp/MorphAnalysis/Form1.cs
@@ -139,16 +139,16 @@ namespace MorphAnalysis
         //Відображення морфологічної таблиці
         private void buttonMorphTable_Click(object sender, EventArgs e)
         {
-            if (cacheData.getListSolutionOfFunctionMorphTable.Count > 0
-                || cacheData.getListFunctionMorphTable.Count > 0)
+            if (cacheData.GetList<SolutionsOfFunction>(true).Count > 0
+                || cacheData.GetList<Function>(true).Count > 0)
                 new MorphTable().Show();
         }
 
         //Відображення таблиці оцінювання тех. рішень відповідно до параметрів цілей
         private void buttonSolParamTable_Click(object sender, EventArgs e)
         {
-            if (cacheData.getListParameterGoalForTables.Count > 0
-                || cacheData.getListSolutionOfFunctionMorphTable.Count > 0)
+            if (cacheData.GetList<ParametersGoal>(true).Count > 0
+                || cacheData.GetList<SolutionsOfFunction>(true).Count > 0)
                 new TableParametersGoalsSolutions().Show();
         }
     }
