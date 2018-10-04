4cf10df9b8a017de8f10b8c4c76e4a84015b10a9
Andrey <andreyks95@gmail.com>
2018-10-04 13:42:33 +0300

    calculate and show in dgv final estimates of solutions

--- GenerationNewObjectsApp/GenerationNewObjectsApp/MorphAnalysis/Form1.cs
+++ GenerationNewObjectsApp/GenerationNewObjectsApp/MorphAnalysis/Form1.cs
@@ -76,6 +76,8 @@ namespace MorphAnalysis
             DisabledEnabledButtons();
         }
 
+        #region 1-ша вкладка (Експертна оцінка)
+
         #region Таблиці для фільтрації (відбору) елементів 
 
         //Відображення таблиці для оцінювання експертами функцій та їх рішень
@@ -94,6 +96,7 @@ namespace MorphAnalysis
             new TableParametersOfGoals(value).Show();
             buttonParamsGoals.Enabled = false;
             buttonSolParamTable.Enabled = true;
+            buttonModParamTable.Enabled = true;
         }
 
         #endregion
@@ -139,6 +142,7 @@ namespace MorphAnalysis
         {
             buttonSolsFuncs.Enabled = enabled;
             buttonParamsGoals.Enabled = enabled;
+            buttonModParamTable.Enabled = enabled;
         }
 
         #endregion
@@ -169,8 +173,112 @@ namespace MorphAnalysis
                 new TableParametersGoalsModifications().Show();
         }
 
+
         #endregion
 
+        #endregion
+
+
+        #region 2-га вкладка (Оцінки рішень) 
+
+        private void buttonSols_Click(object sender, EventArgs e)
+        {
+            List<SolutionsOfFunction> listSolsOfFuncs = cacheData.GetListElements<SolutionsOfFunction>(true);
+            List<ParametersGoalsForModification> listParamsGoalsForMod = cacheData.GetListElements<ParametersGoalsForModification>();
+            List<ParametersGoalsForSolution> listParamsGoalsForSol = cacheData.GetListElements<ParametersGoalsForSolution>();
+
+            if (listSolsOfFuncs.Count > 0 && listParamsGoalsForMod.Count > 0 && listParamsGoalsForSol.Count > 0)
+            {
+                BuildDGV(listSolsOfFuncs, listParamsGoalsForMod, listParamsGoalsForSol);  
+            }
+            else
+            {
+                MessageBox.Show("Елементи списків рішень по: функціям, параметрам цілей, а також список модифікацій по параметрам цілей " +
+                    " для розрахунку оцінок рішень повинні бути  > 0", "Помилка");
+                return;
+            }
+
+
+        }
+
+        private void BuildDGV(List<SolutionsOfFunction> listSolsOfFuncs,
+                                List<ParametersGoalsForModification> listParamsGoalsForMod,
+                                List<ParametersGoalsForSolution> listParamsGoalsForSol)
+        {
+            var dgv = dataGridView1;
+            dgv.Rows.Clear();
+            dgv.Columns.Clear();
+
+            FinalSolutionEstimate finalEstimates = FinalSolutionEstimate.GetInstance();
+
+            finalEstimates.SetEstimates<SolutionsOfFunction>(listSolsOfFuncs);
+            finalEstimates.SetEstimates<ParametersGoalsForModification>(listParamsGoalsForMod);
+            finalEstimates.SetEstimates<ParametersGoalsForSolution>(listParamsGoalsForSol);
+
+            //Оцінка рішення згідно параметрам цілей
+            var dictParamsGoalsForSolution = finalEstimates.GetEstimatesBy<ParametersGoalsForSolution>();
+            //Оцінка рішення згідно функціям
+            var dictSolsOfFuncs = finalEstimates.GetEstimatesBy<SolutionsOfFunction>();
+            //Оцінка рішення згідно параметрам цілей з модифікаціями
+            var dictParamsGoalsForSolutionWithModification = finalEstimates.GetEstimatesBy<ParametersGoalsForSolution>();
+            //Сукупна оцінка рішення
+            var dictFinalSol = finalEstimates.GetFinalSolutionsEstimates;
+
+            List<Solution> sols = GetSolutionsForTable(listSolsOfFuncs, listParamsGoalsForSol);
+
+
+
+            dgv.Columns.Add("Sol", "Рішення");
+            dgv.Columns.Add("vSolParam", "Оцінка рішення згідно параметрам цілей");
+            dgv.Columns.Add("vSolParamWithMod", "Оцінка рішення згідно параметрам цілей з модифікаціями");
+            dgv.Columns.Add("vSolFunc", "Оцінка рішення згідно функціям");
+            dgv.Columns.Add("vSolFinal", "Сукупна оцінка рішення");
+
+            foreach (Solution sol in sols)
+            {
+                string[] row = new string[5];
+                int id = sol.id_solution;
+
+                row[0] = sol.name;
+
+                if (dictParamsGoalsForSolution.ContainsKey(id))
+                    row[1] = dictParamsGoalsForSolution[id].ToString();
+                else
+                    row[1] = "";
+
+                if (dictParamsGoalsForSolutionWithModification.ContainsKey(id))
+                    row[2] = dictParamsGoalsForSolutionWithModification[id].ToString();
+                else
+                    row[2] = "";
+
+                if (dictSolsOfFuncs.ContainsKey(id))
+                    row[3] = dictSolsOfFuncs[id].ToString();
+                else
+                    row[3] = "";
+
+                if (dictFinalSol.ContainsKey(id))
+                    row[4] = dictFinalSol[id].ToString();
+                else
+                    row[4] = "";
+
+                dgv.Rows.Add(row);
+            }
+
+            finalEstimates.Dispose();
+           
+        }
+
+        //Для пошуку усіх оцінених рішень експертами
+        private List<Solution> GetSolutionsForTable(List<SolutionsOfFunction> listSolsOfFuncs, List<ParametersGoalsForSolution> listParamsGoalsForSol)
+        {
+            List<Solution> firstList = listSolsOfFuncs.Select(s => s.Solution).ToList();
+            List<Solution> secondList = listParamsGoalsForSol.Select(s => s.Solution).ToList();
+
+            List<Solution> result = firstList.Union(secondList).GroupBy(sol => sol.id_solution).Select(grp => grp.First()).ToList();
+            return result;
+        }
+
+        #endregion
 
     }
 }
