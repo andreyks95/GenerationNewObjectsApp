d68d3fc53374e5adbba40ffebeead9dfd1a9ed02
Andrey <andreyks95@gmail.com>
2018-10-06 17:55:54 +0300

    Add class for writing and testing how it works before added to real project

---
+++ GenerationNewObjectsApp/GeneticAlgorithm_GeneticSharpLib/ConvertToFromChromosome.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithm_GeneticSharpLib
+{
+
+    //Тестовий клас для побудови, оцінювання хромосоми 
+    public class ConverterToFromChromosome
+    {
+
+        public ConverterToFromChromosome()
+        {
+
+        }
+
+        //Побудувати псевдо хромосому
+        public string BuildChromosomeString(string[] funcsId, string[] solsId, string[] modsId)
+        {
+
+            //example: m1m2m3 block
+            string mod = string.Join("", modsId);
+
+            //example: s1m1m2m3; s2m1m2m3
+            for (int i = 0; i < solsId.Length; i++)
+            {
+                solsId[i] = solsId[i] + mod;
+            }
+
+            //example: s1m1m2m3s2m1m2m3 block
+            string solsMods = string.Join("", solsId);
+
+            //example f1s1m1m2m3s2m1m2m3; f2s1m1m2m3s2m1m2m3; f3s1m1m2m3s2m1m2m3
+            for (int i = 0; i < funcsId.Length; i++)
+            {
+                funcsId[i] = funcsId[i] + solsMods;
+            }
+
+            //example f1s1m1m2m3s2m1m2m3f2s1m1m2m3s2m1m2m3f3s1m1m2m3s2m1m2m3 block
+            string funcsSolsMods = string.Join("", funcsId);
+
+            return funcsSolsMods;
+        }
+
+    }
+}
