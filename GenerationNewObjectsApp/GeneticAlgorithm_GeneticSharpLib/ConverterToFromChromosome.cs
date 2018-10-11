411a630b1494e1642556218ce56b566ed0355883
Andrey <andreyks95@gmail.com>
2018-10-11 11:06:17 +0300

    Add tests methods for convert number to chromosome which depend of count of solution and count of function
    and add method for try get id solutions from array numbers of solutions in row table

--- GenerationNewObjectsApp/GeneticAlgorithm_GeneticSharpLib/ConverterToFromChromosome.cs
+++ GenerationNewObjectsApp/GeneticAlgorithm_GeneticSharpLib/ConverterToFromChromosome.cs
@@ -16,6 +16,63 @@ namespace GeneticAlgorithm_GeneticSharpLib
 
         }
 
+        //Бінарний максимальний розмір блоку для розміщення рішень
+        public int GetSizeBlock(int countOfSolution)
+        {
+            string str = Convert.ToString(countOfSolution, 2);
+            return str.Length;
+        }
+
+        //Бінарний розмір хромосоми (блок * кількість функцій)
+        public int GetSizeChromosome(int countOfSolution, int countOfFunction)
+        {
+            int sizeBlock = GetSizeBlock(countOfSolution);
+            return sizeBlock * countOfFunction;
+        }
+
+        //Повертає масив НОМЕРІВ рішень (а не ID рішень) для словника, з розміру хромосоми 
+        public int[] ConvertFromChromosome(string chromosomeAsString, int countOfFunction)
+        {
+            int sizeChromosome = chromosomeAsString.Length; //Convert.ToInt32(chromosomeAsString, 2);
+
+            //int blockLength = sizeChromosome / countOfFunction;
+
+            //кількість функцій = кількості рішень
+            int[] solutionsNumber = new int[countOfFunction];
+
+            var arr =  Split(chromosomeAsString, countOfFunction);
+
+            int i = 0;
+            foreach (var item in arr)
+            {
+                solutionsNumber[i++] = Convert.ToInt32(item, 2);
+            }
+
+            return solutionsNumber;
+        }
+
+        //Для потрібненння хромосоми на рівні шматки
+        private IEnumerable<string> Split(string str, int chunkSize)
+        {
+            return Enumerable.Range(0, str.Length / chunkSize)
+                .Select(i => str.Substring(i * chunkSize, chunkSize));
+        }
+
+
+        //Знайти id рішень в словнику 
+        public int[] GetIdSolutions(int[] numbersSolutions, Dictionary<int, decimal> dict)
+        {
+            int[] ids = new int[numbersSolutions.Length];
+
+            //Так як, прийшли номера рішень (їх індекси в словнику)
+            //то потрібно знайти ці рішення (їх id) в словнику по індексу
+            for (int i = 0; i < numbersSolutions.Length; i++)
+            {
+                ids[i] = dict.ElementAt(numbersSolutions[i]).Key;
+            }
+            return ids;
+
+        }
 
         /*
         //Побудувати псевдо хромосому
