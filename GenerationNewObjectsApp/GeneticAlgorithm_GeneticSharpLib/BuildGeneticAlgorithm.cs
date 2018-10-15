5a0c63dd4030547ded80c3a1228286f51fe0a5fb
Andrey <andreyks95@gmail.com>
2018-10-15 16:21:24 +0300

    delete comments

--- GenerationNewObjectsApp/GeneticAlgorithm_GeneticSharpLib/BuildGeneticAlgorithm.cs
+++ GenerationNewObjectsApp/GeneticAlgorithm_GeneticSharpLib/BuildGeneticAlgorithm.cs
@@ -219,10 +219,6 @@ namespace GeneticAlgorithm_GeneticSharpLib
             return ga;
         }
 
-        //Оптимальний варіант 
-        //Подумати як передати туди контрол з форми і який!!!
-        //Можливо навіть таблицю, щоби можно було виводити по шапці стовбцям назви функцій
-        //а по строкам ії рішення і суммарну оцінку фітнесс функції
         public void GenerationRan(GeneticSharp.Domain.GeneticAlgorithm ga)
         {
             var latestFitness = 0.0;
