09f34fc6b2d00d988e16466c8a81822fe3466b12
Andrey <andreyks95@gmail.com>
2018-10-15 16:19:17 +0300

    add show result GA for show result working of GA

---
+++ GenerationNewObjectsApp/GenerationNewObjectsApp/MorphAnalysis/HelperForms/ShowResultGA.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace MorphAnalysis.HelperForms
+{
+    partial class ShowResultGA
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resultGA_DGV = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.resultGA_DGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // resultGA_DGV
+            // 
+            this.resultGA_DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.resultGA_DGV.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.resultGA_DGV.Location = new System.Drawing.Point(0, 0);
+            this.resultGA_DGV.Name = "resultGA_DGV";
+            this.resultGA_DGV.Size = new System.Drawing.Size(800, 450);
+            this.resultGA_DGV.TabIndex = 0;
+            // 
+            // ShowResultGA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultGA_DGV);
+            this.Name = "ShowResultGA";
+            this.Text = "ShowResultGA";
+            this.Load += new System.EventHandler(this.ShowResultGA_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.resultGA_DGV)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView resultGA_DGV;
+    }
+}
---
+++ GenerationNewObjectsApp/GenerationNewObjectsApp/MorphAnalysis/HelperForms/ShowResultGA.cs
@@ -0,0 +1,91 @@
+﻿using MorphAnalysis.GeneticAlgorithm;
+using MorphAnalysis.HelperClasses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MorphAnalysis.HelperForms
+{
+    public partial class ShowResultGA : Form
+    {
+        //для зберігання номерів  поколінь, їх кращих хромосом та оцінок
+        private Dictionary<int, double> resultGAValueDict; //= new Dictionary<int, double>();
+        private Dictionary<int, string> resultGAChromosomeDict; //= new Dictionary<int, string>();
+
+        private CacheData cacheData = CacheData.GetInstance();
+
+        List<SolutionsOfFunction> solOfFuncList; //= cacheData.GetListElements<SolutionsOfFunction>(true);
+        
+        List<Function> funcList; //= cacheData.GetListElements<Function>(true);
+
+        ConverterToFromChromosome converter = new ConverterToFromChromosome();
+
+        ManagerGA managerGA = ManagerGA.GetInstance();
+
+        public ShowResultGA()
+        {
+            InitializeComponent();
+        }
+
+
+        public ShowResultGA(Dictionary<int, double> resultValueGADict, Dictionary<int, string> resultChromosomeGADict) : this()
+        {
+            resultGAValueDict = resultValueGADict;
+            resultGAChromosomeDict = resultChromosomeGADict;
+
+            solOfFuncList = cacheData.GetListElements<SolutionsOfFunction>(true);
+            funcList = cacheData.GetListElements<Function>(true);
+        }
+
+        private void ShowResultGA_Load(object sender, EventArgs e)
+        {
+            //Будуємо стовбці
+
+            resultGA_DGV.Columns.Add("NPop", "№ Покоління");
+
+            foreach (Function func in funcList)
+            {
+                resultGA_DGV.Columns.Add("F" + func.id_function, func.name);
+            }
+
+            resultGA_DGV.Columns.Add("Result", "Оцінка");
+
+            //Будуємо рядки
+
+            foreach (KeyValuePair<int, double> kvp in resultGAValueDict)
+            {
+                string[] row = new string[resultGA_DGV.Columns.Count];
+
+                int i = 0;
+
+                //заповнюємо 1-шу комірку рядка
+                int numberGeneration = kvp.Key;
+                row[i++] = numberGeneration.ToString();
+
+                string viewChromosome = resultGAChromosomeDict[kvp.Key];
+                int[] solutionsNumber = converter.ConvertFromChromosome(viewChromosome, funcList.Count);
+                int[] idSolutions = managerGA.GetIdSolutions(solutionsNumber);
+
+                foreach(int idSol in idSolutions)
+                {
+                    string nameSolution = solOfFuncList.FirstOrDefault(sf => sf.Solution.id_solution == idSol).Solution.name;
+                    row[i++] = nameSolution;
+                }
+
+                //заповнюємо останню комірку рядка
+                double resultValue = kvp.Value;
+                row[i++] = resultValue.ToString();
+
+                resultGA_DGV.Rows.Add(row);
+            }
+
+            
+        }
+    }
+}
